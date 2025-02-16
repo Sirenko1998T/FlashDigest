350cf594805d7d44ac3d21d720a54c57151af08a
Sirenko1998T <sirenko23t@gmail.com>
2025-02-16 14:50:10 +0200

    correct message variable and return page text on getText action

--- content.js
+++ content.js
@@ -1,1 +1,6 @@
- 
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+   if (message.action === 'getText') {
+      let text = document.body.innerText || '';
+      sendResponse({ text: text });
+   }
+});
