8c9dca51af7a71980c33ce674e9e33d0bcc81188
Sirenko1998T <sirenko23t@gmail.com>
2025-02-16 14:43:50 +0200

    load saved text from Chrome storage on page load

--- editor.js
+++ editor.js
@@ -1,1 +1,7 @@
- 
+document.addEventListener('DOMContentLoaded',()=>{
+   chrome.storage.local.get('processGetText', (data) => {
+      if(data.processGetText){
+         document.getElementById('texarea').value = data.processGetText;
+      }
+   })
+})
