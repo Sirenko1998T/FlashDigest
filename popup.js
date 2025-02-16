f4c41ac158cb1641e9c558bee92155f61b146923
Sirenko1998T <sirenko23t@gmail.com>
2025-02-16 14:31:43 +0200

    Added click event listener for the button with id ai Retrieved the active tab using chrome.tabs.query Executed content.js on the active tab via chrome.scripting.executeScript Sent a message to the content script to get the page text (getText) Opened editor.html and stored the extracted text using chrome.storage.local.set

--- popup.js
+++ popup.js
@@ -1,1 +1,27 @@
- 
+document.getElementById('ai').addEventListener('click', () => {
+   chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+      if (tabs.length < 1) return; 
+
+      chrome.scripting.executeScript(
+         {
+            target: { tabId: tabs[0].id },
+            files: ['content.js']
+         },
+         () => {
+            chrome.runtime.sendMessage(tabs[0].id, { action: 'getText'}, (response)=>{
+               if(response && response.text){
+                  openEditor(response.text);
+               }
+            })
+         }
+      );
+   });
+});
+
+
+function openEditor (text){
+   let editorUrl = chrome.runtime.getURL('editor.html');
+   chrome.storage.local.set({'processGetText':text},()=>{
+      chrome.tabs.create({url: editorUrl});
+   })
+}
