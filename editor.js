2b226546fcbf8815928eb2c73a454121738943be
Sirenko1998T <sirenko23t@gmail.com>
2025-02-17 13:42:09 +0200

    save and update answer in textarea

--- editor.js
+++ editor.js
@@ -1,7 +1,13 @@
-document.addEventListener('DOMContentLoaded',()=>{
+document.addEventListener('DOMContentLoaded', () => {
    chrome.storage.local.get('processGetText', (data) => {
-      if(data.processGetText){
-         document.getElementById('textarea').value = data.processGetText;
-      }
-   })
-})
+       if (data.processGetText) {
+        let answerArr =    Array.isArray(data.processGetText)? data.processGetText : [data.processGetText];
+        document.getElementById("textarea").value += '\n' + answerArr.join("\n");
+       }
+   });
+});
+ chrome.storage.onChanged.addListener((changes, namespace)=>{
+   let newValue = changes.processGetText.newValue;
+   let answerArr =    Array.isArray(newValue)? newValue : [newValue];
+   document.getElementById("textarea").value += '\n' + answerArr.join("\n");
+ })
